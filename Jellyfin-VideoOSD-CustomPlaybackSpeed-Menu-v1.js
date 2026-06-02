62dff45b506a416a1b1ffae45757b068359914a8
chrissix666 <49721911+chrissix666@users.noreply.github.com>
2026-06-02 03:17:18 +0200

    Update and rename Jellyfin-VideoOSD-CustomPlaybackSpeeds-Menu-v1.js to Jellyfin-VideoOSD-CustomPlaybackSpeed-Menu-v1.js

--- Jellyfin-VideoOSD-CustomPlaybackSpeed-Menu-v1.js
+++ Jellyfin-VideoOSD-CustomPlaybackSpeed-Menu-v1.js
@@ -5,7 +5,7 @@
 (function () {
     const SPEEDS = [0.1, 0.25, 0.33, 0.5, 0.66, 0.75, 1, 1.25, 1.5, 1.75, 2, 2.5, 3, 3.5, 4, 5, 10];
 
-    window.JellyfinCustomPlaybackSpeeds = {
+    window.JellyfinCustomPlaybackSpeed = {
         SPEEDS: SPEEDS
     };
 
